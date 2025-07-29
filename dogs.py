e3419e42d6c229ef7388241808ded0515b8280e1
Pro100den80 <db9656883@gmail.com>
2025-07-29 22:01:59 +0300

    Сздана функция загрузки ссылки на изображение

--- dogs.py
+++ dogs.py
@@ -7,6 +7,20 @@ from io import BytesIO
 
 
 
+def get_dog_image():
+    try:
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        response.raise_for_status()
+        data = response.json()
+        return data('message')
+    except Exception as e:
+        mb.showerror('Ошибка', f'Возникла ошибка: {e}')
+        return None
+
+
+
+
+
 def show_image():
     image_url = get_dog_image()
     if image_url:
@@ -15,7 +29,8 @@ def show_image():
             response.raise_for_status()
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
-            img.thumbnail((300,300))
+            img.thumbnail((300, 300))
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
         except Exception as e:
@@ -29,5 +44,10 @@ window = Tk()
 window.title('Картинки с собачками')
 window.geometry('360x420')
 
-label = Label(text='Загрузить изображение',compound=show_image)
+label = Label()
 label.pack(pady=10)
+
+button = Button(text='Загрузить изображение', command=show_image)
+button.pack(pady=10)
+
+window.mainloop()
